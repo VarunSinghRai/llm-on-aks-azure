e3fdca466ff334c89c113beda8c14c0b83b94326
unknown <varun.singh.rai24@gmail.com>
2026-05-24 15:04:45 +0530

    Terraform modules for acr, llm-aks, keyvault, monitoring, virtualnetwork, rg

---
+++ terraform/modules/keyvault/keyvault.tf
@@ -0,0 +1,11 @@
+resource "azurerm_key_vault" "aks_kv" {
+    name                        = var.name
+    location                    = var.location
+    resource_group_name         = var.resource_group_name
+    tenant_id                   = var.tenant_id
+    sku_name                    = var.sku_name
+    soft_delete_enabled         = var.soft_delete_enabled
+    purge_protection_enabled    = var.purge_protection_enabled
+    
+    tags = var.tags
+}
